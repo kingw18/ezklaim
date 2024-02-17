ad7d9cdc9bc5522346fe5d5d9bece72bcd967644
Ajay Krishna KV <55079486+AJAYK-01@users.noreply.github.com>
2024-02-17 18:54:32 +0000

    port ui to web from vite project

---
+++ apps/web/components/Header.tsx
@@ -0,0 +1,26 @@
+import { Button, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
+import { ConnectKitButton } from "connectkit";
+
+export default function Header() {
+    return (
+        <Navbar className="bg-zinc-900" >
+            <NavbarBrand >
+                <div className="text-3xl">
+                    eZKclaim
+                </div>
+            </NavbarBrand>
+            <NavbarContent >
+                <div className="flex justify-end w-full gap-8">
+                    <NavbarItem>
+                        <Button>
+                            <a href="/api/auth/login">Login</a>
+                        </Button>
+                    </NavbarItem>
+                    <NavbarItem >
+                        <ConnectKitButton />
+                    </NavbarItem>
+                </div>
+            </NavbarContent>
+        </Navbar>
+    );
+}
---
+++ apps/web/components/Home.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import Header from "../components/Header";
+import InputCard from "../components/InputCard";
+
+export default function Home() {
+    return (
+        <div className="flex flex-col h-screen w-screen bg-zinc-600" >
+            <div className="w-full flex ">
+                <Header />
+            </div>
+            <div className="h-full w-full flex justify-center items-center" >
+                <InputCard />
+            </div>
+        </div>
+    );
+}
