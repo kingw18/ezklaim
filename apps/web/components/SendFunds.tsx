7e71b4f88dbf70d02c4da6e879d7a59dee418ef1
Ajay Krishna KV <55079486+AJAYK-01@users.noreply.github.com>
2024-02-17 19:55:13 +0000

    add fund field

--- apps/web/components/SendFunds.tsx
+++ apps/web/components/SendFunds.tsx
@@ -4,6 +4,7 @@ export default function SendFunds() {
     return (
         <div className="mt-10 ml-4 mr-4 mb-10 flex flex-col h-full justify-between">
             <Input type="email" label="Email" placeholder="Enter Receiver email" />
+            <Input type="number" label="Amont" placeholder="Enter amount to send" />
             <div className=" w-full flex justify-center">
                 <Button className="w-1/2" >Send Fund</Button>
             </div>
